2de2a263302c421330da1b6791ad02d4d9f7f99e
Shaxzod <tursunmuratoshaxzod@gmail.com>
2022-10-25 22:05:17 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// //1
+// let str = 'aaa@bbb@ccc'
+
+// //2
+// let stR = "HELLO WORLD"
+
+// //3
+// let hello = 'Hello, it is HTML'
+
+// //4
+// let name = 'alex'
+
+// //5
+// console.log(Math.random(
+    
+// )) 
+
+// //6
+// let text = "Lex Luter has a big suit"
+
+// console.log(
+//     str.replace('@', '!',)
+// )
+
+// console.log(
+//     hello.slice(0,13) + "not JS"
+// )
+
+// console.log(
+//     stR.slice(0,1).toUpperCase() + stR.slice(1).toLowerCase(),
+// )
+
+// console.log(
+//     name.slice(0,1).toUpperCase() + name.slice(1)
+// )
+
+// console.log(
+
+// )
+
+
+// console.log(
+//     text[text.search('a')].toUpperCase() +
+//     text[text.search('L')] +
+//     text[text.search('e')] +
+//     text[text.search('x')] 
+// )
+
+
+
+
+let int = prompt('any numbers')
+
+if(int % 2 == 0 && int != 0) {
+    console.log('countable');
+}  else {
+    console.log('uncuntable');
+}
+
+
+
+let name = 'Alex'
+let age = 26
+
+if(name === 'Alex' && age >= 18 ) {
+    console.log('WELCOME');
+} else {
+    console.log('go home');
+}
